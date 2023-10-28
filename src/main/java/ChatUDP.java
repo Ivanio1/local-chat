0dd81f13d0501536e7935f37ca2582d90580d6f7
Ivanio1 <sobolev1van@yandex.ru>
2023-10-28 16:38:16 +0300

    Network work

--- src/main/java/ChatUDP.java
+++ src/main/java/ChatUDP.java
@@ -1,5 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.nio.charset.StandardCharsets;
 
 public class ChatUDP extends JFrame {
     private JTextArea textAreaMain;
@@ -20,6 +25,18 @@ public class ChatUDP extends JFrame {
         }
     }
 
+    private void btnSendHandler() throws Exception {
+        DatagramSocket  sendSocket = new DatagramSocket();
+        InetAddress IPAdress = InetAddress.getByName(IP_BROADCAST);
+        byte[] sendData;
+        String sentence = textFieldMessage.getText();
+        textFieldMessage.setText("");
+        sendData = sentence.getBytes(StandardCharsets.UTF_8);
+        DatagramPacket sendPacket = new DatagramPacket(sendData,sendData.length, IPAdress, PORT);
+        sendSocket.send(sendPacket);
+
+    }
+
     private void frameDraw(JFrame frame) {
         textFieldMessage = new JTextField();
         textAreaMain = new JTextArea(FRAME_HEIGHT / 19, 50);
@@ -30,15 +47,19 @@ public class ChatUDP extends JFrame {
         JButton btnSend = new JButton();
         btnSend.setText("Send");
         btnSend.setToolTipText("Broadcast a message");
-        btnSend.addActionListener(e ->{
-            textAreaMain.append("YES");
+        btnSend.addActionListener(e -> {
+            try {
+                btnSendHandler();
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
         });
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setTitle(FRAME_TITLE);
-        frame.setLocation(FRAME_LOC_X,FRAME_LOC_Y);
-        frame.setSize(FRAME_WIDTH,FRAME_HEIGHT);
+        frame.setLocation(FRAME_LOC_X, FRAME_LOC_Y);
+        frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
         frame.setResizable(false);
-        frame.getContentPane().add(BorderLayout.NORTH,scrollPane);
+        frame.getContentPane().add(BorderLayout.NORTH, scrollPane);
         frame.getContentPane().add(BorderLayout.CENTER, textFieldMessage);
         frame.getContentPane().add(BorderLayout.EAST, btnSend);
         frame.setVisible(true);
